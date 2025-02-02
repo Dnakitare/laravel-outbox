3f345ebcb0200fdd9493e4796f87d85086313b1e
Daniel Nakitare <dnakitare@gmail.com>
2025-02-02 16:48:39 -0700

    add exceptions

---
+++ src/Exceptions/OutboxExceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Laravel\Outbox\Exceptions;
+
+class OutboxException extends \RuntimeException {}
---
+++ src/Exceptions/TransactionException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Laravel\Outbox\Exceptions;
+
+use RuntimeException;
+
+class TransactionException extends RuntimeException {}
